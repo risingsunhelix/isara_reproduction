5b836280acb5a0a81b28841112b38a2d275ae6a5
risingsunhelix <65543485+risingsunhelix@users.noreply.github.com>
2020-05-23 11:34:38 +0900

    Initial commit
    The reproduction of the isara's website.

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+
+$(function(){
+
+
+    $('.faq-list-item').click(function() {
+  var $answer = $(this).find('.answer');
+  if($answer.hasClass('open')) {
+    $answer.removeClass('open');
+    // slideUpメソッドを用いて、$answerを隠してください
+    $answer.slideUp();
+    $(this).find('span').html('<i class="fas fa-chevron-down"></i>');
+    // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+  } else {
+    $answer.addClass('open');
+    // slideDownメソッドを用いて、$answerを表示してください
+    $answer.slideDown();
+    $(this).find('span').html('<i class="fas fa-chevron-up"></i>');
+    // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+  }
+});
+
+$('.flow-list-item').click(function() {
+var $detail = $(this).find('.flow-list-detail');
+if($detail.hasClass('open')) {
+$detail.removeClass('open');
+// slideUpメソッドを用いて、$answerを隠してください
+$detail.slideUp();
+$(this).find('span').html('<i class="fas fa-chevron-down"></i>');
+// 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+} else {
+$detail.addClass('open');
+// slideDownメソッドを用いて、$answerを表示してください
+$detail.slideDown();
+$(this).find('span').html('<i class="fas fa-chevron-up"></i>');
+// 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+}
+});
+
+
+
+$('#scroll').click(function(){
+  $('html, body').animate({scrollTop:0}, 'slow');
+
+});
+
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>300){
+    $('#scroll').fadeIn();
+  }else{
+    $('#scroll').fadeOut();
+  }
+});
+
+$('#request-btn').click(function(){
+  var form =$('.form').offset().top;
+  $('html, body').animate({scrollTop:form-60}, 'slow');
+  return false;
+});
+$('#request-btn2').click(function(){
+  var form =$('.form').offset().top;
+  $('html, body').animate({scrollTop:form-60}, 'slow');
+  return false;
+});
+
+$('#request-btn3').click(function(){
+  var form =$('.form').offset().top;
+  $('html, body').animate({scrollTop:form-60}, 'slow');
+  return false;
+});
+
+});
